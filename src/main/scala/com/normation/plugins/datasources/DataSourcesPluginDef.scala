34aea1077f34e5abdaf88eb3455352aa4559ba8b
Francois @fanf42 Armand <francois.armand@normation.com>
2017-02-21 12:26:38 +0100

    Fixes #10242: Add a configuration file for plugin build/version related information

--- src/main/scala/com/normation/plugins/datasources/DataSourcesPluginDef.scala
+++ src/main/scala/com/normation/plugins/datasources/DataSourcesPluginDef.scala
@@ -18,20 +18,36 @@ import bootstrap.rudder.plugin.DataSourcesPluginConf
 import net.liftweb.http.LiftRules
 import bootstrap.rudder.plugin.DatasourcesConf
 import net.liftweb.http.ResourceServer
+import java.util.Properties
+import java.io.FileInputStream
 
 class DataSourcesPluginDef() extends RudderPluginDef with Loggable {
 
-  val name = PluginName("data sources")
-  val basePackage = "com.normation.plugins.datasources"
-  val version = PluginVersion(1,0,0)
-  val description : NodeSeq  =
+  val properties = {
+    val props = new Properties
+    props.load(this.getClass.getClassLoader.getResourceAsStream("build.conf"))
+    import scala.collection.JavaConverters._
+    props.asScala
+  }
+
+  override val basePackage = "com.normation.plugins.datasources"
+  override val name = PluginName(properties("plugin-id"))
+  override val displayName = properties("plugin-name")
+  override val version = {
+    PluginVersion.from(properties("plugin-version")).getOrElse(
+      //a version name that indicate an erro
+      PluginVersion(0,0,1, "ERROR-PARSING-VERSION")
+    )
+  }
+
+  override val description : NodeSeq  =
     <div>
      Data source plugin allows to get node properties from third parties provider accessible via a REST API.
      Configuration can be done in <a href="/secure/administration/dataSourceManagement">the dedicated management page</a>
     </div>
 
   def init = {
-    PluginLogger.info("loading 'datasources' plugin")
+    PluginLogger.info(s"loading '${properties("plugin-id")}' plugin")
     LiftRules.statelessDispatch.append(DatasourcesConf.dataSourceApi9)
     // resources in src/main/resources/toserve must be allowed:
     ResourceServer.allow{
