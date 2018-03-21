e6495ffe0571b59b86230e870b7918b83e15d45a
Francois @fanf42 Armand <francois.armand@normation.com>
2018-03-21 23:50:22 +0100

    Fixes #12257: maven furious. maven requires more life sacrifices to be happy

--- makefiles/global-vars.mk
+++ makefiles/global-vars.mk
@@ -7,9 +7,28 @@ MAIN_BUILD          = $(shell  [ -f main-build.conf ] && echo '.' || echo '..')/
 RUDDER_BRANCH       = $(shell sed -ne '/^rudder-branch=/s/rudder-branch=//p' $(MAIN_BUILD))
 LIB_COMMON_VERSION  = $(shell sed -ne '/^lib-common=/s/lib-common=//p' $(MAIN_BUILD))
 LIB_PRIVATE_VERSION = $(shell sed -ne '/^lib-common-private=/s/lib-common-private=//p' $(MAIN_BUILD))
+PARENT_VERSION      = $(shell sed -ne '/^parent-plugin=/s/parent-plugin=//p' $(MAIN_BUILD))
+
+
+ifneq (,$(wildcard ./build.conf))
+PLUGIN_BRANCH   = $(shell sed -ne '/^plugin-branch=/s/plugin-branch=//p' build.conf)
+VERSION         = $(RUDDER_BRANCH)-$(PLUGIN_BRANCH)
+NAME            = $(shell sed -ne '/^plugin-name=/s/plugin-name=//p' build.conf)
+FULL_NAME       = rudder-plugin-$(NAME)
+endif
 
 LIB_COMMON_NAME = plugins-common
 LIB_PRIVATE_NAME = plugins-common-private
 
-MAVEN_OPTS = --batch-mode -U -Drudder-branch="$(RUDDER_BRANCH)" -Dlib-common="$(LIB_COMMON_VERSION)" -Dlib-common-private="$(LIB_PRIVATE_VERSION)"
+MAVEN_OPTS = --batch-mode # -Drudder-branch="$(RUDDER_BRANCH)" -Dlib-common="$(LIB_COMMON_VERSION)" -Dlib-common-private="$(LIB_PRIVATE_VERSION)" -Dparent-version="$(PARENT_VERSION)"
+
+
+generate-pom:
+	cp pom-template.xml pom.xml
+	sed -i -e "s/\$${rudder-branch}/$(RUDDER_BRANCH)/" pom.xml
+	sed -i -e "s/\$${parent-version}/$(PARENT_VERSION)/" pom.xml
+	sed -i -e "s/\$${lib-common}/$(LIB_COMMON_VERSION)/" pom.xml
+	sed -i -e "s/\$${lib-common-private}/$(LIB_PRIVATE_VERSION)/" pom.xml
+	sed -i -e "s/\$${plugin-version}/$(VERSION)/" pom.xml
+
 
--- makefiles/make-plugins-common.mk
+++ makefiles/make-plugins-common.mk
@@ -8,17 +8,26 @@ include ../makefiles/global-vars.mk
 
 
 # maven local repo
-MAVEN_LOCAL_REPO =  $(shell mvn help:effective-settings | grep localRepository | sed -e "s/.*>\(.*\)<.*/\1/")
+MAVEN_LOCAL_REPO =  $(shell mvn help:evaluate -Dexpression="localRepository" | grep basedir | sed -e "s/.*>\(.*\)<.*/\1/")
 
 NAME = $(LIB_$(LIB_TYPE)_NAME)
 VERSION = $(RUDDER_BRANCH)-$(LIB_$(LIB_TYPE)_VERSION)
 PLUGINS_JAR_PATH = $(MAVEN_LOCAL_REPO)/com/normation/plugins/$(NAME)/$(VERSION)/$(NAME)-$(VERSION).jar
+PARENT_V = $(RUDDER_BRANCH)-$(PARENT_VERSION)
+PARENT_POM = $(MAVEN_LOCAL_REPO)/com/normation/plugins/plugins-parent/$(PARENT_V)/plugins-parent-$(PARENT_V).pom
 
 .DEFAULT_GOAL := $(PLUGINS_JAR_PATH)
 
 # maven is such a pain that "mvn install" can't be used with parametrized version (it
 # uses the literal '${plugin-version}' string in path). So we are doing installation by 
 # hand. Yep, most beautiful tool.
-$(PLUGINS_JAR_PATH):
+$(PLUGINS_JAR_PATH): $(PARENT_POM)
 	test -n "$(LIB_TYPE)"  # $$LIB_TYPE must be defined to COMMON or PRIVATE
 	mvn $(MAVEN_OPTS) clean install
+
+$(PARENT_POM):
+	echo $(PARENT_POM)
+	cd .. && mvn $(MAVEN_OPTS) -pl com.normation.plugins:plugins-parent  install
+clean:
+	rm -f  pom.xml
+	rm -rf target
