9fb37ac355c7204d52a75e6c5ce73d85cd603f92
Francois @fanf42 Armand <francois.armand@normation.com>
2018-08-29 12:04:59 +0200

    Fixes #13321:  Update build so that licensed binaries don't embed key/license (branche 4.3)

--- datasources/src/main/scala-templates/limited/com/normation/plugins/datasources/EnablePluginImpl.scala
+++ datasources/src/main/scala-templates/limited/com/normation/plugins/datasources/EnablePluginImpl.scala
@@ -43,6 +43,6 @@ final class CheckRudderPluginEnableImpl() extends LicensedPluginCheck {
   // here are processed variables
   def pluginResourcePublickey = "${plugin-resource-publickey}"
   def pluginResourceLicense   = "${plugin-resource-license}"
-  def pluginDeclaredVersion = "${plugin-declared-version}"
-  def pluginId              = "${plugin-fullname}"
+  def pluginDeclaredVersion   = "${plugin-declared-version}"
+  def pluginId                = "${plugin-fullname}"
 }
