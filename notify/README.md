4d3eea8aed0508da954060a19172db4de572140c
Felix Dallidet <felix.dallidet@normation.com>
2018-11-16 18:56:40 +0100

    Fixes #13854: Make a feature to open ticket based on rudder events

--- notify/README.md
+++ notify/README.md
@@ -32,23 +32,32 @@ Installation
   /opt/rudder/bin/rudderpkg install-file <plugin>.rpkg
   ```
 
-- Edit the configuration file in /opt/rudder/etc/notify.conf. This config file sets which notifications are enabled
+- Edit the configuration file in `/opt/rudder/etc/notify.conf`. This config file sets which notifications are enabled
   and some additional parameters.
 
 Usage
 -----
 
 The plugin has to be started as a daemon and will watch for non-compliances on your Rudder infrastructure.
-Start it with `/opt/rudder/bin/rudder-notifyd start`.
+Start it with `systemctl start rudder-notify`.
 
-The plugins takes its config from /opt/rudder/etc/notify.conf. Edit this file to your preferences.
+The plugins takes its config from `/opt/rudder/etc/notify.conf`. Edit this file to your preferences.
 
 For the e-mail plugin, the plugin uses the `mail` utility to send its notifications. This program needs to be installed
 and properly set up. The e-mail notifications can be set in the conf file to not spam you, and send by batches of a certain
 period for you to set. The conf file also contains the recipients of the e-mails.
 
-The slack module needs slack webhooks to be able to send slack messages to users or channels. You will have to declare an app
-for your workspace and create these webhooks via your slack admin panel. More info here : https://api.slack.com/incoming-webhooks 
+The Slack module needs Slack webhooks to be able to send Slack messages to users or channels. You will have to declare an app
+for your workspace and create these webhooks via your Slack admin panel. More info here : https://api.slack.com/incoming-webhooks 
+
+
+GLPI Integration
+-------
+
+The plugin can be configure to open ticket in GLPI based on non-compliance in Rudder.
+To do so, in `/opt/rudder/etc/notify.conf`, set the GLPI plugin `on` and fulfill the required parameters.
+* `userToken` can be found in the `Users settings` page in the GLPI interface under the `APIToken` field.
+* `apiToken` and `url` can be found under the `Setup -> General -> API` pagae in the GLPI interface.
 
 Authors
 -------
---
+++ notify/share/python/glpi.py
@@ -0,0 +1,147 @@
+import requests, json
+
+# TODO clean initalizing to create the adequate ticket
+
+# Class used to create the php filter to look for objects in the glpi api
+class criterias:
+  def __init__(self):
+    self.array = []
+
+  def addCriteria(self, criteria):
+    self.array.append(criteria)
+
+  def __str__(self):
+    output = ""
+    for iCriteria in range(len(self.array)):
+      output += (self.array[iCriteria]).show(iCriteria) + "&"
+    return output[:len(output)-1]
+      
+class criteria:
+  def __init__(self, field, searchType, value, link="AND"):
+    self.link = link
+    self.field = field
+    self.searchtype = searchType
+    self.value = value
+
+  def show(self, i):
+    sep = "criteria[" + str(i) + "]"
+    return sep + "[link]=" + self.link + "&" + sep + "[field]=" + self.field + "&" + sep + "[searchtype]=" + self.searchtype + "&" + sep + "[value]=" + self.value
+    
+
+
+# Class used to create a ticket in the glpi api
+class glpiTicket:
+  def __init__(self, **fields): 
+    self.__dict__ = fields
+
+  def __init__(self, name, content, status=1, urgency=3, impact=3, priority=3):
+    self.name = name
+    self.content = content
+    self.status = status
+    self.urgency = urgency
+    self.impact = impact
+    self.priority = priority
+#    self.entities_id = entities_id
+#    self.requesttypes_id = requesttypes_id
+#    self.itilcategories_id = itilcategories_id 
+#    self.type = type
+#    self.global_validation = global_validation 
+
+  def build(self):
+    return { key:value for key, value in self.__dict__.items() if not key.startswith('__') and not callable(key) }
+
+# Class used to create and parse the api queries
+class glpiSession:
+  def __init__(self, userToken, apiToken, baseUrl):
+    self.userToken = userToken
+    self.apiToken  = apiToken
+    self.baseUrl   = baseUrl
+    self.headers   = ''
+    self.sessionToken = ''
+    self.field = {
+      "content": "21",
+       "status": "12",
+         "name": "1",
+         "type": "14"
+      }
+    self.status = {
+      "assigned": "2",
+       "planned": "3",
+       "pending": "4",
+        "solved": "5",
+        "closed": "6",
+           "new": "1"
+      }
+
+  def initSession(self):
+    headers = { 'Content-Type': 'application/json', 'Authorization': "user_token "+self.userToken, 'App-Token': self.apiToken }
+    response = requests.get(self.baseUrl + '/initSession', headers=headers)
+    if (self.controlCode(['200'], response)):
+      self.sessionToken = response.json()['session_token']
+      self.headers = { 'Content-Type': 'application/json', 'Session-Token': self.sessionToken, 'App-Token': self.apiToken }
+
+  def killSession(self):
+    response = requests.get(self.baseUrl + '/killSession', headers=self.headers)
+    self.controlCode(['200'], response)
+    exit()
+
+  def openTicket(self, name, description):
+    data = { "input": glpiTicket(name, description).build() }
+    response = requests.post(self.baseUrl + '/Ticket' , headers=self.headers, data=json.dumps(data))
+    if (self.controlCode(['201'], response)):
+      return 0
+
+  def getTickets(self):
+    response = requests.get(self.baseUrl + '/Ticket' , headers=self.headers)
+    if (self.controlCode(['200'], response)):
+      return response.json()
+
+  def getTicket(self, ticketID):
+    response = requests.get(self.baseUrl + '/Ticket/' + ticketID , headers=self.headers)
+    if (self.controlCode(['200'], response)):
+      return response.json()
+
+  def searchTicket(self, name, content=None, ticket_status=None):
+    filters = criterias()
+    # Filter on Name
+    nameCriteria = criteria(self.field['name'], "contains", name, link="AND")
+    filters.addCriteria(nameCriteria)
+    # Filter on Description
+    if (content != None):
+      contentCriteria = criteria(self.field['content'], "contains", content, link="AND")
+      filters.addCriteria(contentCriteria)
+    # Filter on ticket_status
+    if (ticket_status != None):
+      statusCriteria = criteria(self.field['status'], "equals", self.status[ticket_status], link="AND")
+      filters.addCriteria(statusCriteria)
+
+    response = requests.get(self.baseUrl + '/search/Ticket?' + str(filters), headers=self.headers)
+    if (self.controlCode(['200'], response)):
+      return response.json()
+
+  def similarTicketExists(self, ticket, ticket_status=None):
+    if (ticket_status != None):
+      for iStatus in ticket_status:
+        if (self.searchTicket(ticket.name, ticket.content, iStatus)['totalcount'] >= 1):
+          return True
+      return False
+    else:
+      if (self.searchTicket(ticket.name, ticket.content, ticket_status)['totalcount'] >= 1):
+        return True
+      else:
+        return False
+
+  def customGETRequest(self, url):
+    response = requests.get(self.baseUrl + url , headers=self.headers)
+    if (self.controlCode(['200'], response)):
+      return (response.text.encode('utf-8'))
+    
+  def controlCode(self, expectedCodes, response):
+    if (str(response.status_code) in expectedCodes):
+      return True
+    else:
+      print(response.text.encode('utf-8'))
+      print("Something went wrong while calling the GLPI API, received error code " + str(response.status_code))
+      self.killSession()
+      return False
+
