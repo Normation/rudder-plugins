0aaab67f15e0fd3acc1f2bfdc38a98e3f9ef6061
Nabil El khalii <nel@rudder.io>
2019-06-11 10:43:23 +0200

    Fixes #14835: zabbix plugin done (developement and documentation)

--- zabbix/bin/zabbix-plugin.py
+++ zabbix/bin/zabbix-plugin.py
@@ -6,8 +6,8 @@ Usage:
     zabbix-plugin.py hook addHost <id>
     zabbix-plugin.py hook rmHost <host>
     zabbix-plugin.py apply-configuration
-
 """
+
 # -*- coding: utf-8 -*-
 import traceback
 import os
@@ -23,7 +23,6 @@ from docopt import docopt
 from pyzabbix import ZabbixAPI
 from pprint import pprint
 from requests.exceptions import HTTPError
-
 
 
 # Used to determine which IP will be given to Zabbix, from all the IPs Rudder sends us (any v4 that's not 127.0.0.1)
@@ -42,13 +41,12 @@ canonifyMacroKey = lambda key : "{$" + key.upper() + "}"
 
 # Generic call to the Rudder API, completed by its path param. Using URL and token obtained from zabbix.conf
 def getRequestToRudderAPI(conf, path):
-        try:
-            data = requests.get(conf["RUDDER"]["rudderAPIURL"] + path, headers={ 'X-API-Token': conf['RUDDER']['rudderAPIToken'] }, verify=False).json()
-        except:
-            print("[!] Error : check your Rudder API token")
-            sys.exit(-1)
-            return data
-
+    try:
+        data = requests.get(conf["RUDDER"]["rudderAPIURL"] + path, headers={ 'X-API-Token': conf['RUDDER']['rudderAPIToken'] }, verify=False).json()
+    except:
+        print("[!] Error : check your Rudder API token")
+        sys.exit(-1)
+    return data
 
 def addZabbixHost(zapi, node):
     nodeip = ""
@@ -56,36 +54,30 @@ def addZabbixHost(zapi, node):
         if validateIPAddr(ip):
             nodeip = ip
             break
-
     zapi.host.create(host=node["hostname"], groups=[{"groupid":zabbixGroupID(zapi, "Rudder nodes")}], interfaces=[{"type":"1", "main":"1", "useip":"1", "ip":nodeip, "dns":"", "port":"10050"}],description=node["id"]) 
 
-
 def update(conf, register, zapi):
-
-        zhosts = zapi.host.get()
-        zmacros = zapi.do_request("usermacro.get", params=["selectHosts"])
-        rnodes = getRequestToRudderAPI(conf, "/nodes")["data"]["nodes"]
+    zhosts = zapi.host.get()
+    zmacros = zapi.do_request("usermacro.get", params=["selectHosts"])
+    rnodes = getRequestToRudderAPI(conf, "/nodes")["data"]["nodes"]
      
     # Add all nodes from Rudder to Zabbix and to the register upon addition
-        for node in rnodes:
-            if node["hostname"] not in (h["host"] for h in zhosts):
-                addZabbixHost(zapi, node)
-                print("[ ] Adding node " + node["hostname"] +" to Zabbix...")
-            if not register.has_section(node["hostname"]):
-                register.add_section(node["hostname"])
+    for node in rnodes:
+        if node["hostname"] not in (h["host"] for h in zhosts):
+            addZabbixHost(zapi, node)
+            print("[ ] Adding node " + node["hostname"] +" to Zabbix...")
+        if not register.has_section(node["hostname"]):
+            register.add_section(node["hostname"])
 
     # Delete every Zabbix host that's not in Rudder, but in the register; then delete it from the register
-        for host in zhosts:
-            if host["host"] not in (node["hostname"] for node in rnodes) and host["host"] in register.sections():
-                print("[ ] Removing node " + host["host"] + " from Zabbix...")
-                zapi.host.delete(zabbixHostID(host["host"], zhosts))
-                register.remove_section(host["host"])
+    for host in zhosts:
+        if host["host"] not in (node["hostname"] for node in rnodes) and host["host"] in register.sections():
+            print("[ ] Removing node " + host["host"] + " from Zabbix...")
+            zapi.host.delete(zabbixHostID(host["host"], zhosts))
+            register.remove_section(host["host"])
 
     
-
-
 # The ConfigParser lowercases all keys and section names, and Zabbix uppercases macro keys, hence this overriding to keep things nice
-
 class MyConfigParser(configparser.ConfigParser):
     optionxform = lambda self, optionstr : optionstr
 
@@ -101,7 +93,6 @@ if __name__ == "__main__":
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     
     try:
-        
         zapi = ZabbixAPI(conf['ZABBIX']['ZabbixWebserviceURL'])
         zapi.login(conf['ZABBIX']['username'], conf['ZABBIX']['password'])
         print("Connected to Zabbix API Version %s" % zapi.api_version())             
@@ -119,7 +110,6 @@ if __name__ == "__main__":
         print("[+] Done.")
 
     # adding new rudder node in zabbix hosts
-        
     elif args["hook"]:
         if args["addHost"]: 
             node = getRequestToRudderAPI(conf, "/nodes/" + args["<id>"])["data"]["nodes"][0]
@@ -131,7 +121,6 @@ if __name__ == "__main__":
                     register.add_section(node["hostname"])
 
         # delete host from zabbix if host is absent in rudder server
-
         elif args["rmHost"]:
             zhosts = zapi.host.get()
             rnodes = getRequestToRudderAPI(conf, "/nodes")["data"]["nodes"]
@@ -143,7 +132,6 @@ if __name__ == "__main__":
                         register.remove_section(host["host"])
 
     # apply-configuration ( hosts - Templates - Macros) 
-
     elif args["apply-configuration"]:
         rootPath = '/var/rudder/shared-files/root/files'
         zabbixCsv = '/var/rudder/shared-files'
@@ -153,23 +141,18 @@ if __name__ == "__main__":
         
         # save all rudder nodes related templates to a csv file " /var/rudder/shared-files/rudderMonitor.csv "
         os.chdir('/var/rudder/shared-files')
-        
         with open('rudderMonitor.csv', 'wb') as myfile:
             if os.path.exists('rudderMonitor.csv'):
                 c = csv.writer(open("rudderMonitor.csv", "wb"))
-
             else:
                 print('[!] Node ' + name + ' has no rudder monitoring config file, considering it empty...')
             
         for host in zapi.host.get(output="extend"):
             if host['host'] in (node["hostname"] for node in rnodes):
-                
                 rtemplates = zapi.do_request("host.get", params={"output":"extend", "selectParentTemplates": ["templateid", "name"], "hostids": host['hostid'] })
                 rmacros = zapi.do_request("host.get", params={"output":"extend", "selectMacros": ["hostmacroid", "macro", "value"], "hostids": host['hostid'] }) 
                 ztemplates = zapi.template.get()
                 
-                
-
                 for result in rtemplates["result"]:
                     for parentTemplate in result['parentTemplates']:
                         c.writerow([host['host'],parentTemplate["name"]])
@@ -184,14 +167,11 @@ if __name__ == "__main__":
                             print('[!] Node ' + result['host'] + ' does not exist among rudder nodes')
 
 
-        
-
         if os.path.exists(rootPath):
             for name in os.listdir(rootPath):
                 if name in (node["id"] for node in rnodes):
                     print('[!] '  + name + ' is an id for Rudder node applying configuration ... ')
                     
-
             for dir in next(os.walk(rootPath))[1]:
                 csvfile = rootPath + '/' + dir + '/rudder_monitoring.csv'
                 if os.path.exists(csvfile):
@@ -199,7 +179,6 @@ if __name__ == "__main__":
                         # the file must stay open
                         fd = open(csvfile)
                         confcsv = csv.reader(fd)
-
                         conversionID = getRequestToRudderAPI(conf, "/nodes")["data"]["nodes"]
                         if name in (node["id"] for node in conversionID):
                             node = getRequestToRudderAPI(conf, "/nodes/" + name) 
@@ -221,12 +200,10 @@ if __name__ == "__main__":
                         # In RUDDER, we can edit technique editors, assign them to a node via rule and eventually monitor these directives by zabbix. 
                         
                         for row in confcsv:
-
                             # Templates
                             if (row[0] == 'template'):
                                 template = zapi.do_request("template.get",params={"output":"extend", "filter": { "host": [row[1]]}})
                                 rudderListTmp.append(row[1])
-                                
                                 
                                 # When monitoring template is added to the node, we link this zabbix template to the host zabbix.
                                 for result in rtemplates['result']:
@@ -236,7 +213,6 @@ if __name__ == "__main__":
                                         linkTemplate = zapi.do_request("template.massadd", params={"output":"extend", "templates": [{ "templateid": templateidADD }], "hosts":[{"hostid": hostid }]})
                                         print('[ ] Applying conf : the host ' + hostid + ' is linked to the template ' + templateidADD + ' ...') 
                             
-                            
                             # Macros
                             if (row[0] == 'param'):
                                 rudderListTmp.append(row[1])
@@ -244,7 +220,6 @@ if __name__ == "__main__":
                                 rudderListTmpMacros[-1] = "{$"+rudderListTmpMacros[-1].upper()+"}"
                                 
                                 # When monitoring parameter is added to the node, we create a host macro in zabbix
-
                                 for result in rmacros['result']:
                                     if not any(m['macro'] == row[1] for m in result['macros']):
                                         try:
@@ -253,12 +228,10 @@ if __name__ == "__main__":
                                         except:
                                             print('[ ] Macro ' + row[1] + ' already added to node ' + result['host'] + ' ...')
                                    
-                            print(json.dumps(rtemplates, indent=4))
                             for result in rtemplates["result"]:
                                 for parentTemplate in result['parentTemplates']:
                                     nameT = parentTemplate["name"]
                         
-                            
                             for result in rmacros["result"]:
                                 for m in result['macros']:
                                     nameM = m['macro']
@@ -283,16 +256,14 @@ if __name__ == "__main__":
                                 unlinkTemplate = zapi.do_request("template.massremove", params={"output":"extend", "templateids": templateidRM , "hostids": hostid })   
                                 print('[ ] Applying conf: the host ' + hostid + ' is unlinked to the template ' + templateidRM + ' ...')
                              
-                                
                         # When a monitoring parameter is removed from rudder node, we remove macro from zabbix host 
                         for result in rmacros['result']:
                             for m in result['macros']:
-                                listnames.append(m)
-                            
-                            for ligne in listnames:
+                                listnames.append(m)    
+                            for line in listnames:
                                 for item in resMacros:
-                                    if (ligne['macro']==item):
-                                        macroid = ligne['hostmacroid']
+                                    if (line['macro']==item):
+                                        macroid = line['hostmacroid']
                                         if(resMacros):
                                              deleteUserMacros = zapi.do_request("usermacro.delete", params=[ macroid ])
                                              print('[ ] Applying conf : Macro ' + item + ' is removed to the host ' + result['host'] + ' ...' )
@@ -302,9 +273,5 @@ if __name__ == "__main__":
                 else:
                     print('[!] Node ' + name + ' has no rudder monitoring config file, considering it empty...')
         
-
-                    
-                
-                
 
 register.write(open(registerFile, "w")) 
