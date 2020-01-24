88438e91adde7c74472a761d89789de5b76c81ff
Felix Dallidet <felix.dallidet@normation.com>
2020-01-24 17:53:39 +0100

    Fixes #16613: Rename and update the plugin

---
+++ ansible-policies/tools/ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PLAYBOOK="$1"
+PID="$2"
+DIRECTIVE_ID="$3"
+
+ansible-playbook $PLAYBOOK > /var/rudder/tmp/ansible_policies_${PID}_${DIRECTIVE_ID} 2>/dev/null
+if grep -qE "failed=[1-9][0-9]*" /var/rudder/tmp/ansible_policies_${PID}_${DIRECTIVE_ID}; then
+    exit 1
+elif grep -qE "changed=[1-9][0-9]*" /var/rudder/tmp/ansible_policies_${PID}_${DIRECTIVE_ID}; then
+    exit 2
+fi
+exit 0
