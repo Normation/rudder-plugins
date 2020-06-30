953c3c648c913dc6814f4ac9f11bf7ee82534183
Vincent Membré <vincent.membre@normation.com>
2020-06-30 21:48:41 +0200

    Fixes #17886: Optimize elm plugins

--- src/main/elm/build-app.sh
+++ src/main/elm/build-app.sh
@@ -4,4 +4,4 @@
 # whatever the path from which this script is called
 ELM_DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $ELM_DIR
-elm make sources/auth-backends.elm --output=generated/auth-backends.js
+elm make --optimize sources/auth-backends.elm --output=generated/auth-backends.js
