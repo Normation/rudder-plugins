5415167b4e427f6272fb3722cc432059362d85ec
Francois @fanf42 Armand <francois.armand@normation.com>
2022-02-08 20:51:17 +0100

    Fixes #20735: Counterpart of 20734: update import for RudderUserDetails

--- change-validation/src/main/scala/com/normation/plugins/changevalidation/comet/WorkflowInformation.scala
+++ change-validation/src/main/scala/com/normation/plugins/changevalidation/comet/WorkflowInformation.scala
@@ -43,7 +43,7 @@ import com.normation.plugins.changevalidation.TwoValidationStepsWorkflowServiceI
 import com.normation.rudder.AuthorizationType
 import com.normation.rudder.services.workflows.WorkflowService
 import com.normation.rudder.services.workflows.WorkflowUpdate
-import com.normation.rudder.web.model.CurrentUser
+import com.normation.rudder.web.services.CurrentUser
 import net.liftweb.common._
 import net.liftweb.http._
 
