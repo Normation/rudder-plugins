5b07c7f32ef408584d1103ac26196ef1e44df6b1
Francois @fanf42 Armand <francois@fanf.eu>
2022-10-20 23:13:26 +0200

    Fixes #21999: Auto-format scala code (plugins branch 7.1)

--- src/main/scala/com/normation/plugins/authbackends/AuthBackendsPluginDef.scala
+++ src/main/scala/com/normation/plugins/authbackends/AuthBackendsPluginDef.scala
@@ -1,39 +1,39 @@
 /*
-*************************************************************************************
-* Copyright 2018 Normation SAS
-*************************************************************************************
-*
-* This file is part of Rudder.
-*
-* Rudder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* In accordance with the terms of section 7 (7. Additional Terms.) of
-* the GNU General Public License version 3, the copyright holders add
-* the following Additional permissions:
-* Notwithstanding to the terms of section 5 (5. Conveying Modified Source
-* Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
-* Public License version 3, when you create a Related Module, this
-* Related Module is not considered as a part of the work and may be
-* distributed under the license agreement of your choice.
-* A "Related Module" means a set of sources files including their
-* documentation that, without modification of the Source Code, enables
-* supplementary functions or services in addition to those offered by
-* the Software.
-*
-* Rudder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************************
+ * Copyright 2018 Normation SAS
+ *************************************************************************************
+ *
+ * This file is part of Rudder.
+ *
+ * Rudder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In accordance with the terms of section 7 (7. Additional Terms.) of
+ * the GNU General Public License version 3, the copyright holders add
+ * the following Additional permissions:
+ * Notwithstanding to the terms of section 5 (5. Conveying Modified Source
+ * Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
+ * Public License version 3, when you create a Related Module, this
+ * Related Module is not considered as a part of the work and may be
+ * distributed under the license agreement of your choice.
+ * A "Related Module" means a set of sources files including their
+ * documentation that, without modification of the Source Code, enables
+ * supplementary functions or services in addition to those offered by
+ * the Software.
+ *
+ * Rudder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
 
-*
-*************************************************************************************
-*/
+ *
+ *************************************************************************************
+ */
 
 package com.normation.plugins.authbackends
 
@@ -56,7 +56,7 @@ class AuthBackendsPluginDef(override val status: PluginStatus) extends DefaultPl
 
   def init = {}
 
-  def oneTimeInit : Unit = {}
+  def oneTimeInit: Unit = {}
 
   // there is no config file as the main rudder-web.properties must be modified
   // for these properties.
@@ -65,11 +65,12 @@ class AuthBackendsPluginDef(override val status: PluginStatus) extends DefaultPl
   override def apis: Option[LiftApiModuleProvider[_ <: EndpointSchema]] = Some(AuthBackendsConf.api)
 
   override def pluginMenuEntry: Option[Menu] = {
-    Some(Menu("authBackensdManagement", <span>Authentication backends</span>) /
+    Some(
+      Menu("authBackensdManagement", <span>Authentication backends</span>) /
       "secure" / "plugins" / "authBackendsManagement"
       >> LocGroup("pluginsGroup")
-      >> TestAccess ( () => Boot.userIsAllowed("/secure/index", Administration.Read))
-      >> Template(() => ClasspathTemplates("template" :: "AuthBackendsManagement" :: Nil ) openOr <div>Template not found</div>)
+      >> TestAccess(() => Boot.userIsAllowed("/secure/index", Administration.Read))
+      >> Template(() => ClasspathTemplates("template" :: "AuthBackendsManagement" :: Nil) openOr <div>Template not found</div>)
     )
   }
 }
--- src/main/scala/com/normation/plugins/authbackends/api/AuthBackendsApi.scala
+++ src/main/scala/com/normation/plugins/authbackends/api/AuthBackendsApi.scala
@@ -1,48 +1,48 @@
 /*
-*************************************************************************************
-* Copyright 2018 Normation SAS
-*************************************************************************************
-*
-* This file is part of Rudder.
-*
-* Rudder is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* In accordance with the terms of section 7 (7. Additional Terms.) of
-* the GNU General Public License version 3, the copyright holders add
-* the following Additional permissions:
-* Notwithstanding to the terms of section 5 (5. Conveying Modified Source
-* Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
-* Public License version 3, when you create a Related Module, this
-* Related Module is not considered as a part of the work and may be
-* distributed under the license agreement of your choice.
-* A "Related Module" means a set of sources files including their
-* documentation that, without modification of the Source Code, enables
-* supplementary functions or services in addition to those offered by
-* the Software.
-*
-* Rudder is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************************
+ * Copyright 2018 Normation SAS
+ *************************************************************************************
+ *
+ * This file is part of Rudder.
+ *
+ * Rudder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In accordance with the terms of section 7 (7. Additional Terms.) of
+ * the GNU General Public License version 3, the copyright holders add
+ * the following Additional permissions:
+ * Notwithstanding to the terms of section 5 (5. Conveying Modified Source
+ * Versions) and 6 (6. Conveying Non-Source Forms.) of the GNU General
+ * Public License version 3, when you create a Related Module, this
+ * Related Module is not considered as a part of the work and may be
+ * distributed under the license agreement of your choice.
+ * A "Related Module" means a set of sources files including their
+ * documentation that, without modification of the Source Code, enables
+ * supplementary functions or services in addition to those offered by
+ * the Software.
+ *
+ * Rudder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rudder.  If not, see <http://www.gnu.org/licenses/>.
 
-*
-*************************************************************************************
-*/
+ *
+ *************************************************************************************
+ */
 
 package com.normation.plugins.authbackends.api
 
 import com.normation.plugins.authbackends.AuthBackendsRepository
 import com.normation.plugins.authbackends.JsonSerialization
+import com.normation.rudder.api.ApiVersion
 import com.normation.rudder.api.HttpAction.GET
 import com.normation.rudder.rest.ApiModuleProvider
 import com.normation.rudder.rest.ApiPath
-import com.normation.rudder.api.ApiVersion
 import com.normation.rudder.rest.AuthzToken
 import com.normation.rudder.rest.EndpointSchema
 import com.normation.rudder.rest.EndpointSchema.syntax._
@@ -58,11 +58,9 @@ import com.normation.rudder.rest.lift.LiftApiModule0
 import com.normation.rudder.rest.lift.LiftApiModuleProvider
 import net.liftweb.http.LiftResponse
 import net.liftweb.http.Req
-import net.liftweb.json.NoTypeHints
 import net.liftweb.json._
+import net.liftweb.json.NoTypeHints
 import sourcecode.Line
-
-
 
 /*
  * This file contains the internal API used to discuss with the JS application.
@@ -70,23 +68,22 @@ import sourcecode.Line
  * It gives the list of currently configured authentication backends.
  */
 sealed trait AuthBackendsApi extends EndpointSchema with InternalApi with SortIndex
-object AuthBackendsApi extends ApiModuleProvider[AuthBackendsApi] {
+object AuthBackendsApi       extends ApiModuleProvider[AuthBackendsApi] {
 
   final case object GetAuthenticationInformation extends AuthBackendsApi with ZeroParam with StartsAtVersion10 {
-    val z = implicitly[Line].value
+    val z              = implicitly[Line].value
     val description    = "Get information about current authentication configuration"
     val (action, path) = GET / "authbackends" / "current-configuration"
 
     override def dataContainer: Option[String] = None
   }
 
-  def endpoints = ca.mrvisser.sealerate.values[AuthBackendsApi].toList.sortBy( _.z )
+  def endpoints = ca.mrvisser.sealerate.values[AuthBackendsApi].toList.sortBy(_.z)
 }
 
-
 class AuthBackendsApiImpl(
-    restExtractorService: RestExtractorService
-  , authRepo            : AuthBackendsRepository
+    restExtractorService: RestExtractorService,
+    authRepo:             AuthBackendsRepository
 ) extends LiftApiModuleProvider[AuthBackendsApi] {
   api =>
 
@@ -95,9 +92,13 @@ class AuthBackendsApiImpl(
   def schemas = AuthBackendsApi
 
   def getLiftEndpoints(): List[LiftApiModule] = {
-    AuthBackendsApi.endpoints.map(e => e match {
-        case AuthBackendsApi.GetAuthenticationInformation => GetAuthenticationInformation
-    }).toList
+    AuthBackendsApi.endpoints
+      .map(e => {
+        e match {
+          case AuthBackendsApi.GetAuthenticationInformation => GetAuthenticationInformation
+        }
+      })
+      .toList
   }
 
   /*
@@ -106,7 +107,7 @@ class AuthBackendsApiImpl(
    * enabled ones.
    */
   object GetAuthenticationInformation extends LiftApiModule0 {
-    val schema = AuthBackendsApi.GetAuthenticationInformation
+    val schema        = AuthBackendsApi.GetAuthenticationInformation
     val restExtractor = api.restExtractorService
     def process0(version: ApiVersion, path: ApiPath, req: Req, params: DefaultParams, authzToken: AuthzToken): LiftResponse = {
 
@@ -118,8 +119,8 @@ class AuthBackendsApiImpl(
         config.toJson()
       }) match {
         case Right(json) =>
-           RestUtils.toJsonResponse(None, json)(schema.name, params.prettify)
-        case Left(ex) =>
+          RestUtils.toJsonResponse(None, json)(schema.name, params.prettify)
+        case Left(ex)    =>
           val err = "Error when trying to get group information: " + ex.getMessage()
           RestUtils.toJsonError(None, JString(err))(schema.name, params.prettify)
       }
@@ -127,6 +128,3 @@ class AuthBackendsApiImpl(
   }
 
 }
-
-
-
