b079d954d0c71e1d65f40eac27b4c8e197f80f71
Francois @fanf42 Armand <francois@fanf.eu>
2023-02-09 13:38:19 +0100

    fixup! fixup! Fixes #22349: Update user plugin to manage update custom roles
    Fixes #22349: Update user plugin to manage update custom roles

--- user-management/src/test/scala/com/normation/plugins/usermanagement/RoleComputationTest.scala
+++ user-management/src/test/scala/com/normation/plugins/usermanagement/RoleComputationTest.scala
@@ -16,48 +16,48 @@ class RoleComputationTest extends Specification {
 
   "Computation of Role Coverage over Rights" should {
 
-//    "return 'None' when parameters are empty" in {
-//      (computeRoleCoverage(Set(Role.User), Set()) must beNone) and
-//      (computeRoleCoverage(Set(), Set(AuthorizationType.Compliance.Read)) must beNone) and
-//      (computeRoleCoverage(Set(), Set()) must beNone)
-//    }
-//
-//    "return 'None' when authzs contains no_rights" in {
-//      (computeRoleCoverage(Set(Role.User), Set(AuthorizationType.NoRights)) must beNone) and
-//      (computeRoleCoverage(Set(Role.User), Set(AuthorizationType.NoRights) ++ AuthorizationType.allKind) must beNone)
-//    }
-//
-//    "return a 'Custom' role for empty intersection" in {
-//      computeRoleCoverage(Set(Role.User), Set(AuthorizationType.Compliance.Read)) must beEqualTo(
-//        Some(Set(Role.forAuthz(AuthorizationType.Compliance.Read)))
-//      )
-//    }
-//
-//    "contains 'Inventory' and 'Custom' roles" in {
-//      computeRoleCoverage(
-//        Role.values,
-//        Set(AuthorizationType.Compliance.Read) ++ Role.Inventory.rights.authorizationTypes
-//      ) must beEqualTo(Some(Set(Role.Inventory, Role.forAuthz(AuthorizationType.Compliance.Read))))
-//    }
-//
-//    "only detect 'Inventory' role" in {
-//      computeRoleCoverage(
-//        Set(Role.Inventory),
-//        Role.Inventory.rights.authorizationTypes
-//      ) must beEqualTo(Some(Set(Role.Inventory)))
-//    }
-//
-//    "only detect one custom role" in { // why ?
-//      val a: Set[AuthorizationType] = Set(
-//        AuthorizationType.UserAccount.Read,
-//        AuthorizationType.UserAccount.Write,
-//        AuthorizationType.UserAccount.Edit
-//      )
-//      computeRoleCoverage(
-//        Set(Role.User, Role.Inventory),
-//        a
-//      ) must beEqualTo(Some(Set(Role.forAuthz(a))))
-//    }
+    "return 'None' when parameters are empty" in {
+      (computeRoleCoverage(Set(Role.User), Set()) must beNone) and
+      (computeRoleCoverage(Set(), Set(AuthorizationType.Compliance.Read)) must beNone) and
+      (computeRoleCoverage(Set(), Set()) must beNone)
+    }
+
+    "return 'None' when authzs contains no_rights" in {
+      (computeRoleCoverage(Set(Role.User), Set(AuthorizationType.NoRights)) must beNone) and
+      (computeRoleCoverage(Set(Role.User), Set(AuthorizationType.NoRights) ++ AuthorizationType.allKind) must beNone)
+    }
+
+    "return a 'Custom' role for empty intersection" in {
+      computeRoleCoverage(Set(Role.User), Set(AuthorizationType.Compliance.Read)) must beEqualTo(
+        Some(Set(Role.forAuthz(AuthorizationType.Compliance.Read)))
+      )
+    }
+
+    "contains 'Inventory' and 'Custom' roles" in {
+      computeRoleCoverage(
+        Role.values,
+        Set(AuthorizationType.Compliance.Read) ++ Role.Inventory.rights.authorizationTypes
+      ) must beEqualTo(Some(Set(Role.Inventory, Role.forAuthz(AuthorizationType.Compliance.Read))))
+    }
+
+    "only detect 'Inventory' role" in {
+      computeRoleCoverage(
+        Set(Role.Inventory),
+        Role.Inventory.rights.authorizationTypes
+      ) must beEqualTo(Some(Set(Role.Inventory)))
+    }
+
+    "only detect one custom role" in { // why ?
+      val a: Set[AuthorizationType] = Set(
+        AuthorizationType.UserAccount.Read,
+        AuthorizationType.UserAccount.Write,
+        AuthorizationType.UserAccount.Edit
+      )
+      computeRoleCoverage(
+        Set(Role.User, Role.Inventory),
+        a
+      ) must beEqualTo(Some(Set(Role.forAuthz(a))))
+    }
 
     "return administrator " in {
       computeRoleCoverage(
@@ -66,18 +66,18 @@ class RoleComputationTest extends Specification {
       ) must beEqualTo(Some(Set(Role.Administrator)))
     }
 
-//    "allows intersection between know roles" in {
-//      computeRoleCoverage(
-//        Set(Role.Inventory, Role.User),
-//        Role.User.rights.authorizationTypes ++ Role.Inventory.rights.authorizationTypes
-//      ) must beEqualTo(Some(Set(Role.User, Role.Inventory)))
-//    }
-//
-//    "ignore NoRights role" in {
-//      computeRoleCoverage(
-//        Set(Role.NoRights, Role.User),
-//        Role.User.rights.authorizationTypes
-//      ) must beEqualTo(Some(Set(Role.User)))
-//    }
+    "allows intersection between know roles" in {
+      computeRoleCoverage(
+        Set(Role.Inventory, Role.User),
+        Role.User.rights.authorizationTypes ++ Role.Inventory.rights.authorizationTypes
+      ) must beEqualTo(Some(Set(Role.User, Role.Inventory)))
+    }
+
+    "ignore NoRights role" in {
+      computeRoleCoverage(
+        Set(Role.NoRights, Role.User),
+        Role.User.rights.authorizationTypes
+      ) must beEqualTo(Some(Set(Role.User)))
+    }
   }
 }
