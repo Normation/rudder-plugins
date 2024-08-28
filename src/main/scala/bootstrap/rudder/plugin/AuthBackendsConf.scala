a9269732c23c3a8d467f91de60861bdfc347e35c
Francois @fanf42 Armand <francois@fanf.eu>
2024-08-28 22:34:21 +0200

    Fixes #25378: Update to jetty 11 spring 6  on plugins

--- src/main/scala/bootstrap/rudder/plugin/AuthBackendsConf.scala
+++ src/main/scala/bootstrap/rudder/plugin/AuthBackendsConf.scala
@@ -68,8 +68,8 @@ import com.normation.zio.*
 import com.typesafe.config.ConfigException
 import java.net.URI
 import java.util
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.joda.time.DateTime
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
