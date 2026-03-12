38acaebf4e9aecb78b144b8d0cc6949bc0a0431c
Clark Andrianasolo <clark.andrianasolo@rudder.io>
2026-03-12 17:48:29 +0100

    Fixes #28502: Directive change request shows whole sectionval diff

--- change-validation/src/main/elm/sources/ChangeRequestChangesForm.elm
+++ change-validation/src/main/elm/sources/ChangeRequestChangesForm.elm
@@ -695,7 +695,7 @@ displayDirectiveDiff contextPath directive =
         , displayBoolField "System" directive.system
         , displayStringDiff "Long description" directive.longDescription
         , displayDiffField (displayMaybe text) "Policy Mode" directive.policyMode
-        , displayDiffField (\param -> pre [] [ text param ]) "Parameters" directive.parameters
+        , displayDirectiveParametersDiff  "Parameters" directive.parameters
         ]
 
 
